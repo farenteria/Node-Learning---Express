62632b57df189d5fced3031be94a6586a3df12be
Fernando Renteria <rentfern@gmail.com>
2018-03-30 10:49:32 -0500

    added partials for DRY code. added helper functions

--- server.js
+++ server.js
@@ -2,22 +2,30 @@ const express = require("express");
 const hbs = require("hbs");
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
+
+// allows us to use this function within hbs files given the first name 
+hbs.registerHelper("getCurrentYear", () =>{
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper("screamIt", (text) =>{
+    return text.toUpperCase()
+});
 
 // get() allows us to set handler for get requests
 app.get("/", (req, res) => {
     res.render("home.hbs",{
         pageTitle: "Home Page",
         welcomeMessage: "Welcome to the coolest page",
-        currentYear: new Date().getFullYear()
     });
 });
 
 app.get("/about", (req, res) => {
     res.render("about.hbs", {
         pageTitle: "About Page",
-        currentYear: new Date().getFullYear()
     });
 });
 
