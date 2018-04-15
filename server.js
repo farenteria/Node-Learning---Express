161597a71f9e3c5e01097dbedda2bcae19e54e56
Fernando Renteria <rentfern@gmail.com>
2018-04-15 14:26:19 -0500

    Added middleware

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
+
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+// middleware
+// next tells us when function is done
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    // newer versions of node require error callback
+    fs.appendFile("server.log", log + "\n", (err) => {
+        if(err){
+            console.log("Unable to append to server.log");
+        }
+    });
+
+    next(); // necessary to use for application to continue
+});
+
+// app.use((req, res, next) => {
+//     res.render("maintenance.hbs");
+// });
+
 app.use(express.static(__dirname + "/public"));
 
 // allows us to use this function within hbs files given the first name 
-hbs.registerHelper("getCurrentYear", () =>{
+hbs.registerHelper("getCurrentYear", () => {
     return new Date().getFullYear()
 });
 
-hbs.registerHelper("screamIt", (text) =>{
+hbs.registerHelper("screamIt", (text) => {
     return text.toUpperCase()
 });
 
